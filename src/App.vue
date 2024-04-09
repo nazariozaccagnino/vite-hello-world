d51ad16ae77474041d1acbc3f55a5f362fd95a8b
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-04-09 16:21:45 +0200

    created the bonus jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
     <AppTitle />
+    <AppHero />
 </template>
 
 <script>
 import AppTitle from "./components/AppTitle.vue";
+import AppHero from "./components/AppHero.vue";
+
   export default {
     name:'App',
     components:{
       AppTitle,
+      AppHero
     },data(){
 
     },
---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <img src="/images/1_C8xdSMN_IkeXDsTLt6EACA.png" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppHero',
+        data(){
+            return{
+
+            }
+        },
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
